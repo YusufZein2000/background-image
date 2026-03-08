71b2f85313e5a6a0563935e5a47848073ded032a
Yusuf <Yusufzein2000@gmail.com>
2026-03-08 12:24:10 +0400

    made basic css and html started js and had a huge discussoin about color mixing

---
+++ bv.js
@@ -0,0 +1,9 @@
+let hexcode1 = (document.getElementById("color1"))
+let hexcode2 = (document.getElementById("color2"))
+let output = document.getElementById("output")
+
+function combineColors() {
+    hexcode1 = hexcode1.value.toUpperCase()
+    hexcode2 = hexcode2.value.toUpperCase()
+    console.log(hexcode1())
+}
